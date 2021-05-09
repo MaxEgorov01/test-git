48887fb57b3c002b65bae45d7d2ebad652e2302c
MaxEgorov01 <egorov.maksim@b-leader.ru>
2021-05-09 23:04:54 +0400

    add names.php

--- index.php
+++ index.php
@@ -1,7 +1,9 @@
 <?php
 
 require_once "text.php";
+require_once "names.php";
 
+var_dump($names);
 echo $text;
 
 ?>
---
+++ names.php
@@ -0,0 +1,5 @@
+<?php
+
+$names = ['name' => 'Maks', 'surname' => 'Egorov', ];
+
+?>
