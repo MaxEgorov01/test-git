228d26126bd636951b7c2a52603664b74e5bde5f
MaxEgorov01 <egorov.maksim@b-leader.ru>
2021-05-11 20:41:28 +0400

    del text added INFO

--- index.php
+++ index.php
@@ -1,11 +1,9 @@
 <?php
 
-require_once "text.php";
 require_once "names.php";
 require_once "number.php";
 
 echo $number;
 var_dump($names);
-echo $text;
 
 ?>
--- text.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-$text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam leo nisi, venenatis ac sem imperdiet, sollicitudin venenatis orci. Praesent at lacus ipsum. Fusce cursus nulla vitae semper gravida. Sed a sem luctus, ultrices ligula quis, semper sapien. Praesent eget aliquam ante. Integer eu justo et risus dapibus egestas. Sed ante metus, convallis eu faucibus a, malesuada quis eros. Cras rutrum in erat at tincidunt. Nunc maximus eget nisi et elementum.";
-
-?>
